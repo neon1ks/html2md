801aed76d8cda8d7277327e8c895074403d9bbc2
Maksim Demyanov (neon1ks) <neon1ks@mail.ru>
2023-09-11 05:57:21 +0800

    Init project

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "version.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "Version: " << PROJECT_VESRION << endl;
+    return 0;
+}
